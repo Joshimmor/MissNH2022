8d1d9cc6389de38c1c048a6ef77164276fd344ab
Joshua Immordino <joshimmor@Gmail.com>
2022-06-05 12:34:59 -0400

    final

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,16 +1,33 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 import { Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  animations:[
+    trigger('social', [
+      transition('void => *', [
+          style({opacity:0,transform:'translateX(-200px)'}),
+          animate('350ms 100ms ease-in-out')
+      ]),
+      transition('* => void', [
+          animate('350ms ease-in-out',
+              style({opacity:1,transform:'translateX(-200px)'}))
+      ]),
+  ])
+  ]
 })
 export class MenuComponent implements OnInit {
   constructor() { }
+  socialView:boolean = false
   @Output() view = new EventEmitter<boolean>();
   ngOnInit(): void {
   }
   closeDrawer() {
     this.view.emit(false);
+  }
+  showSocial(){
+    this.socialView = !this.socialView
   }
 }
